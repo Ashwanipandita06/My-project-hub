fd944b47df67ae0a499ee3bfca060e26f4ce9ccf
Ashwanipandita06 <65610932+Ashwanipandita06@users.noreply.github.com>
2020-05-19 20:46:58 +0530

    Add files via upload

--- Assessment.py
+++ Assessment.py
@@ -2,16 +2,16 @@
 take_number = int(input("Enter the number"))
 
 
-def factorial_without_loop(number):
+def factorial_with_loop(number):
     f = 1
     for i in range(1, number + 1):
         f = f * i
     return f
 
 
-print(factorial_without_loop(take_number))
+print(factorial_with_loop(take_number))
 
-# Factorial without loop
+# 2) Factorial without loop
 
 take_number1 = int(input("Enter the number"))
 
@@ -24,3 +24,24 @@ def factorial_without_loop (number):
 
 
 print(factorial_without_loop(take_number1))
+
+
+# 3) to find the second highest
+
+list1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 7, 5, 3, 2, 1]
+print("The second highest number in the list is " + str(max(list1)-1))
+
+# 4 Fibonacci
+
+
+
+
+
+
+
+
+
+
+
+
+
